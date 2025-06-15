b8b5f1026c53247cc71f471b9d89077e40c789f5
rtrivgreg <rtrivgreg@yahoo.com>
2025-06-15 19:39:15 -0400

    Update RayTest.py

--- RayTest.py
+++ RayTest.py
@@ -1,8 +1,34 @@
----...
-print("ssssss");
+# read_yaml 
+# https://www.youtube.com/watch?v=fwLBfZFrLgI
+# exception, function
 
-e = 6
-       print("aaaa")
-e +=3
+def my_Process(first_argument):
+    try:
+        with open(sys.argv[1], 'r') as file:
+            stream = file.read()
+            dictionary = yaml.load(stream, Loader=yaml.FullLoader)
+            x = datetime.datetime.now() 
+            console.print("\n"+x.strftime("%c") + "\n" +"Processing: " + first_argument + "\n")
+            for key, value in dictionary.items():
+                console.print(key + " : " + str(value))
+    except:
+        print("An exception occurred")
+#-------------------------------------------------------------
+import datetime
+import sys
+import yaml
 
+#Makes the Output pretty
+from rich.console import Console
+console = Console()
+
+if len(sys.argv) > 1:
+  # User provided at least one argument
+  my_Process(sys.argv[1])
+else:
+  # User did not provide any arguments after script name
+  print("No YML file specified.")
+  sys.exit()
+
+ 
 
